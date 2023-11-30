56b2b90fa6c006ebd0758c7f313a8dd9f64e5e8b
Vasanth1234567 <vasanthprakashvp@gmail.com>
2023-11-30 13:14:01 +0530

    Initial Commit.

---
+++ src/views/home/index.jsx
@@ -0,0 +1,92 @@
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { MessageDisplay } from '@/components/common';
+import { ProductShowcaseGrid } from '@/components/product';
+import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from '@/constants/routes';
+import {
+  useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
+} from '@/hooks';
+import bannerImg from '../../../static/banner.jpg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  useDocumentTitle('PerfectFit | Home');
+  useScrollTop();
+
+  const {
+    featuredProducts,
+    fetchFeaturedProducts,
+    isLoading: isLoadingFeatured,
+    error: errorFeatured
+  } = useFeaturedProducts(6);
+  const {
+    recommendedProducts,
+    fetchRecommendedProducts,
+    isLoading: isLoadingRecommended,
+    error: errorRecommended
+  } = useRecommendedProducts(6);
+
+  return (
+    <main className="content">
+      <div className="home">
+        <div className="banner">
+          <div className="banner-desc">
+            <h1 className="text-thin">
+              <strong>Be</strong>
+              &nbsp;everything with&nbsp;
+              <strong>Fashion</strong>
+            </h1>
+            <p>
+            Don't be into trends. Don't make fashion own you, but you decide what you are, what you want to express by the way you dress and the way to live.
+            </p>
+            <br />
+            <Link to={SHOP} className="button">
+              Shop Now &nbsp;
+              <ArrowRightOutlined />
+            </Link>
+          </div>
+          <div className="banner-img"><img src={bannerImg} alt="" /></div>
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Featured Products</h1>
+            <Link to={FEATURED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorFeatured && !isLoadingFeatured) ? (
+            <MessageDisplay
+              message={errorFeatured}
+              action={fetchFeaturedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={featuredProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Recommended Products</h1>
+            <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorRecommended && !isLoadingRecommended) ? (
+            <MessageDisplay
+              message={errorRecommended}
+              action={fetchRecommendedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={recommendedProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
